567216b7cfa27b54e70a8444a34c88e44b1187cd
Manasseh Mwangi <52884229+ManassehMwangi@users.noreply.github.com>
2023-07-23 20:26:58 +0300

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# because the previous image is running on port 5000 delete the previous image to run the current built image
 # Stop the running container (if any)
 containerid = 'docker ps | awk -F " " '{print $1}''
 docker rm -f $containerid
