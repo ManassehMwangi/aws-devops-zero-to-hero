b0b4d59e4aef66f9421b0cd8643190abfaa5a711
Manasseh Mwangi <52884229+ManassehMwangi@users.noreply.github.com>
2023-07-23 20:47:46 +0300

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,6 +4,5 @@ set -e
 # Pull the Docker image from Docker Hub
 docker pull manasseh9/simple-python-flask-app:latest
 
-
 # Run the Docker image as a container
 docker run -d -p 5000:5000 manasseh9/simple-python-flask-app:latest
